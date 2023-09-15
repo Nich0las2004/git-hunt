6771f5e1b743dea9645f955b918a901100f9759d
Nika Oniani <nikaoniani2018@gmail.com>
2023-09-15 14:30:29 +0400

    removed a theme

--- src/templates/Header/Header.js
+++ src/templates/Header/Header.js
@@ -1,13 +1,11 @@
 import classes from "./Header.module.css";
 
 import Title from "./Title/Title";
-import Mode from "./Mode/Mode";
 
 const Header = () => {
   return (
     <header className={classes.header}>
       <Title />
-      <Mode />
     </header>
   );
 };
